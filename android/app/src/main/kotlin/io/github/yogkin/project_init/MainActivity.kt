1b2125af40edd008aa126ba8dd96249efbc9bdcb
czm <yogkin@163.com>
2023-02-23 23:19:18 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/io/github/yogkin/project_init/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.yogkin.project_init
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return RefreshConfiguration(
+      headerBuilder: () => CustomHeader(
+          height: 100,
+          builder: (context, mode) {
+            if (mode == RefreshStatus.idle) {
+              return const SizedBox.shrink();
+            } else {
+              return Container(
+                alignment: Alignment.bottomCenter,
+                height: 100.0,
+                child: Center(
+                  child: Image.asset(
+                    'assets/images/mjCustomGifImage.gif',
+                  ),
+                ),
+              );
+            }
+          }),
+      child: ScreenUtilInit(
+        builder: (BuildContext context, Widget? child) {
+          return GetMaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: child,
+          );
+        },
+        child: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void _incrementCounter() {
+    //TODO:
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '点击我',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
