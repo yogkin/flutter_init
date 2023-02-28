1a048bce5ad4ce966b83a58ec829f5928141f624
czm <yogkin@163.com>
2023-02-28 19:08:28 +0800

    添加 自定义 SliverPersistentHeaderDelegate

--- lib/sliver_test.dart
+++ lib/sliver_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/src/size_extension.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class SliverTest extends StatefulWidget {
@@ -8,7 +9,8 @@ class SliverTest extends StatefulWidget {
   State<SliverTest> createState() => _SliverTestState();
 }
 
-class _SliverTestState extends State<SliverTest> {
+class _SliverTestState extends State<SliverTest>
+    with SingleTickerProviderStateMixin {
   RefreshController controller = RefreshController();
 
   List<int> items = List.generate(20, (index) => index);
@@ -31,6 +33,18 @@ class _SliverTestState extends State<SliverTest> {
     controller.loadComplete();
   }
 
+  final tabs = [
+    Tab(
+      text: '窝的收益',
+    ),
+    Tab(
+      text: '窝的奖励',
+    )
+  ];
+
+  late final firstTabController =
+      TabController(length: tabs.length, vsync: this);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,31 +60,58 @@ class _SliverTestState extends State<SliverTest> {
             slivers: [
               SliverAppBar(
                 title: Text('sliver test'),
-                pinned: true,
-                expandedHeight: 400,
-                bottom: PreferredSize(
-                  preferredSize: Size.fromHeight(30),
-                  child: Container(
-                    height: 30,
-                    width: double.infinity,
-                    color: Colors.amberAccent,
-                    alignment: Alignment.center,
-                    child: Text('哈哈'),
-                  ),
+                centerTitle: true,
+              ),
+              SliverToBoxAdapter(
+                child: Container(
+                  height: 200.w,
+                  color: Colors.blueGrey,
                 ),
               ),
-              SliverList(delegate: SliverChildBuilderDelegate((context, index) {
-                return Container(
-                  alignment: Alignment.center,
-                  color: Colors.blue[200 + items[index] % 4 * 100],
-                  height: 100 + items[index] % 4 * 20.0,
-                  child: Text('Item: ${items[index]}'),
-                );
-              }))
+              SliverPersistentHeader(delegate: FirstTabView(tabs, controller)),
+              SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return Container(
+                    alignment: Alignment.center,
+                    color: Colors.blue[200 + items[index] % 4 * 100],
+                    height: 100 + items[index] % 4 * 20.0,
+                    child: Text('Item: ${items[index]}'),
+                  );
+                },
+                childCount: items.length,
+              )),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class FirstTabView extends SliverPersistentHeaderDelegate {
+  FirstTabView(this.tabs, this.controller);
+
+  final List<Tab> tabs;
+  final TabController controller;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return TabBar(
+      tabs: tabs,
+      controller: controller,
+    );
+  }
+
+  @override
+  double get maxExtent => 30.w;
+
+  @override
+  double get minExtent => 30.w;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return this==oldDelegate;
   }
 }
