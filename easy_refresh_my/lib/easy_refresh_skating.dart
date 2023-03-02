cb4b926dd87568c41dfad4eab1ac8f3183bc96d4
czm <yogkin@163.com>
2023-03-02 15:39:01 +0800

    初步完成

---
+++ easy_refresh_my/lib/easy_refresh_skating.dart
@@ -0,0 +1,9 @@
+library easy_refresh_my;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
+import 'package:easy_refresh/easy_refresh.dart';
+
+part 'src/skating_indicator.dart';
+part 'src/skating_header.dart';
+part 'src/skating_footer.dart';
---
+++ easy_refresh_my/lib/src/skating_indicator.dart
@@ -0,0 +1,94 @@
+part of easy_refresh_my;
+
+const _kDefaultSkatingTriggerOffset = 180.0;
+
+const _kSkatingProcessed = Duration(milliseconds: 700);
+
+/// Skating indicator.
+/// Base widget for [SkatingHeader] and [SkatingFooter].
+class _SkatingIndicator extends StatefulWidget {
+  /// Indicator properties and state.
+  final IndicatorState state;
+
+  /// True for up and left.
+  /// False for down and right.
+  final bool reverse;
+
+  const _SkatingIndicator({
+    Key? key,
+    required this.state,
+    required this.reverse,
+  }) : super(key: key);
+
+  @override
+  State<_SkatingIndicator> createState() => _SkatingIndicatorState();
+}
+
+class _SkatingIndicatorState extends State<_SkatingIndicator> {
+  double get _offset => widget.state.offset;
+
+
+  /// Mode change listener.
+  void _onModeChange(IndicatorMode mode, double offset) {
+    if (mode == IndicatorMode.ready || mode == IndicatorMode.processing) {
+
+    } else {
+
+    }
+    if (mode == IndicatorMode.processed) {
+
+    } else {
+
+    }
+    if (mode == IndicatorMode.inactive) {
+      setState(() {
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    widget.state.notifier.addModeChangeListener(_onModeChange);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    widget.state.notifier.removeModeChangeListener(_onModeChange);
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant _SkatingIndicator oldWidget) {
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: _offset,
+        ),
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          child: SizedBox(
+            width: double.infinity,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              height: 100.0,
+              child: Center(
+                child: Image.asset(
+                  'assets/images/mjCustomGifImage.gif',
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
